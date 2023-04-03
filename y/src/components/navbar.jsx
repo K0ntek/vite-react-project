f4015b3e74119d894de5b81bb22f7635c24a6307
Kuba <93491047+K0ntek@users.noreply.github.com>
2023-04-03 12:19:17 +0200

    Add files via upload

--- y/src/components/navbar.jsx
+++ y/src/components/navbar.jsx
@@ -8,7 +8,6 @@ import {MdOutlineLocalOffer} from 'react-icons/md'
 import {RiContactsLine} from 'react-icons/ri'
 
 import { Link } from "react-scroll";
-import { InView, useInView } from 'react-intersection-observer';
 
 
 const Navbar =()=>{
@@ -52,7 +51,10 @@ const Navbar =()=>{
         <>
         <div><HiMenuAlt3 className="absolute top-[1%] right-[1%] z-[99] text-white text-[30px] sm:hidden button" onClick={toggleNavbar}/></div>
         <nav className="fixed top-0 right-0 sm:left-0 bg-black w-[80%] sm:w-[70px] h-[100%] z-40 overflow-hidden group sm:hover:w-[200px] transition-all duration-200 navbar">
-            <img src={viteLogo} alt='logo' className="relative top-[1%] left-[50%] translate-x-[-50%]"/>
+            {/* <img src={viteLogo} alt='logo' className="relative top-[1%] left-[50%] translate-x-[-50%]"/> */}
+            <div><h1 className="relative top-[1%] left-[50%] translate-x-[-50%] text-[60px] font-orbitron text-[#DAA520] group-hover:hidden transition-all duration-300">K</h1>
+            <h1 className="relative top-[1%] left-[50%] translate-x-[-50%] text-[35px] font-orbitron text-[#DAA520] hidden group-hover:block transition-all duration-300">KONTEK</h1>
+            <p className="relative top-[1%] left-[50%] translate-x-[-50%] text-[20px] font-orbitron text-gray-500 hidden group-hover:block transition-all duration-300">Lorem, ipsum dolor.</p></div>
             <ul className="justify-center relative top-[50px] sm:left-[15%]">
                 {navElements.map((element)=>{
                     return(
